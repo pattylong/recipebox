b1367cc05d8671b71e34e647a092f90348a20698
Patty Student <patty.long@berkeley.edu>
2020-07-12 16:40:24 -0700

    updating key

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(32)
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'oops-its-not-a-secret'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'postgresql://pattylong:cat@localhost:5432/recipe'
